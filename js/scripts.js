3b68411f71c02688aeb1d4941e8fd7251e5d60f2
Yoon <tmdwo7383@naver.com>
2023-12-10 01:03:39 +0900

    fix modal error by remove -collapse class

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,52 @@
-/*!
- * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
- * Copyright 2013-2023 Start Bootstrap
- * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
- */
-//
-// Scripts
-//
+// /*!
+//  * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+//  * Copyright 2013-2023 Start Bootstrap
+//  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+//  */
+// //
+// // Scripts
+// //
 
-window.addEventListener("DOMContentLoaded", (event) => {
-  // Activate Bootstrap scrollspy on the main nav element
-  const sideNav = document.body.querySelector("#sideNav");
-  if (sideNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: "#sideNav",
-      rootMargin: "0px 0px -40%",
-    });
-  }
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   // Activate Bootstrap scrollspy on the main nav element
+//   const sideNav = document.body.querySelector("#sideNav");
+//   if (sideNav) {
+//     new bootstrap.ScrollSpy(document.body, {
+//       target: "#sideNav",
+//       rootMargin: "0px 0px -40%",
+//     });
+//   }
 
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = [].slice.call(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
-      }
-    });
-  });
-});
-// script.js
+//   // Collapse responsive navbar when toggler is visible
+//   const navbarToggler = document.body.querySelector(".navbar-toggler");
+//   const responsiveNavItems = [].slice.call(
+//     document.querySelectorAll("#navbarResponsive .nav-link")
+//   );
+//   responsiveNavItems.map(function (responsiveNavItem) {
+//     responsiveNavItem.addEventListener("click", () => {
+//       if (window.getComputedStyle(navbarToggler).display !== "none") {
+//         navbarToggler.click();
+//       }
+//     });
+//   });
+// });
+// // script.js
 
-// script.js
+// // script.js
 
-document.getElementById("openModalBtn").addEventListener("click", function () {
-  document.getElementById("myModal").style.display = "block";
-  document.getElementById("modalContent").src = "playerDetail.html";
-});
+// document.getElementById("openModalBtn").addEventListener("click", function () {
+//   document.getElementById("myModal").style.display = "block";
+//   document.getElementById("modalContent").src = "playerDetail.html";
+// });
 
-document
-  .getElementsByClassName("close")[0]
-  .addEventListener("click", function () {
-    document.getElementById("myModal").style.display = "none";
-  });
+// document
+//   .getElementsByClassName("close")[0]
+//   .addEventListener("click", function () {
+//     document.getElementById("myModal").style.display = "none";
+//   });
 
-window.addEventListener("click", function (event) {
-  if (event.target == document.getElementById("myModal")) {
-    document.getElementById("myModal").style.display = "none";
-  }
-});
+// window.addEventListener("click", function (event) {
+//   if (event.target == document.getElementById("myModal")) {
+//     document.getElementById("myModal").style.display = "none";
+//   }
+// });
