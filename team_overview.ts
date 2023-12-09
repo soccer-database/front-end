17fdfa23a31594f1168bc6ae2106a25373c5e448
Yoon <tmdwo7383@naver.com>
2023-12-09 20:21:00 +0900

    temp

--- team_overview.ts
+++ team_overview.ts
@@ -1,139 +1,139 @@
 export const leagues = [
-    {
-        name: "Premier League",
-        teams: [
-            {
-                fullName: "Arsenal Football Club",
-                nickname: "The Gunners",
-                shortName: "Arsenal",
-                founded: "1886-12-01",
-                ground: {
-                    name: "Emirates Stadium",
-                    latitude: 51.5550812,
-                    longitude: -0.1109831,
-                    capacity: 60260
-                },
-                owner: "Kroenke Sports & Entertainment",
-                manager: "Mikel Arteta",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 5},
-                    {fromYear: 2019, toYear: 2020, finalRank: 8},
-                    {fromYear: 2020, toYear: 2021, finalRank: 8},
-                    {fromYear: 2021, toYear: 2022, finalRank: 5},
-                    {fromYear: 2022, toYear: 2023, finalRank: 2}
-                ]
-            },
-            {
-                fullName: "Chelsea Football Club",
-                nickname: "The Blues",
-                shortName: "Chelsea",
-                founded: "1905-03-10",
-                ground: {
-                    name: "Stamford Bridge",
-                    latitude: 51.4816663,
-                    longitude: -0.1935314,
-                    capacity: 40343
-                },
-                owner: "Blues Partners Limited",
-                manager: "Mauricio Pochettino",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 3},
-                    {fromYear: 2019, toYear: 2020, finalRank: 4},
-                    {fromYear: 2020, toYear: 2021, finalRank: 4},
-                    {fromYear: 2021, toYear: 2022, finalRank: 3},
-                    {fromYear: 2022, toYear: 2023, finalRank: 12}                    
-                ]
-            },
-            {
-                fullName: "Liverpool Football Club",
-                nickname: "The Reds",
-                shortName: "Liverpool",
-                founded: "1892-03-15",
-                ground: {
-                    name: "Anfield",
-                    latitude: 53.4343003,
-                    longitude: -2.9658238,
-                    capacity: 53394
-                },
-                owner: "Fenway Sports Group",
-                manager: "Jürgen Klopp",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 2},
-                    {fromYear: 2019, toYear: 2020, finalRank: 1},
-                    {fromYear: 2020, toYear: 2021, finalRank: 3},
-                    {fromYear: 2021, toYear: 2022, finalRank: 2},
-                    {fromYear: 2022, toYear: 2023, finalRank: 5}
-                ]
-            },
-            {
-                fullName: "Manchester City Football Club",
-                nickname: "The Citizens",
-                shortName: "Man City",
-                founded: "1880-11-23",
-                ground: {
-                    name: "Etihad Stadium",
-                    latitude: 53.4831666,
-                    longitude: -2.2029485,
-                    capacity: 53400
-                },
-                owner: "City Football Group Limited",
-                manager: "Pep Guardiola",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 1},
-                    {fromYear: 2019, toYear: 2020, finalRank: 2},
-                    {fromYear: 2020, toYear: 2021, finalRank: 1},
-                    {fromYear: 2021, toYear: 2022, finalRank: 1},
-                    {fromYear: 2022, toYear: 2023, finalRank: 1}
-                ]
-            },
-            {
-                fullName: "Manchester United Football Club",
-                nickname: "The Red Devils",
-                shortName: "Man United",
-                founded: "1878-03-05",
-                ground: {
-                    name: "Old Trafford",
-                    latitude: 53.4597835,
-                    longitude: -2.288024,
-                    capacity: 74879
-                },
-                owner: "Manchester United plc",
-                manager: "Erik ten Hag",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 6},
-                    {fromYear: 2019, toYear: 2020, finalRank: 3},
-                    {fromYear: 2020, toYear: 2021, finalRank: 2},
-                    {fromYear: 2021, toYear: 2022, finalRank: 6},
-                    {fromYear: 2022, toYear: 2023, finalRank: 3}
-                ]
-            },
-            {
-                fullName: "Tottenham Hotspur Football Club",
-                nickname: "The Lilywhites",
-                shortName: "Spurs",
-                founded: "1882-09-05",
-                ground: {
-                    name: "Tottenham Hotspur Stadium",
-                    latitude: 51.6042238,
-                    longitude: -0.068798,
-                    capacity: 62850
-                },
-                owner: "ENIC International Ltd.",
-                manager: "Ange Postecoglou",
-                players: [],
-                rankHistories: [
-                    {fromYear: 2018, toYear: 2019, finalRank: 4},
-                    {fromYear: 2019, toYear: 2020, finalRank: 6},
-                    {fromYear: 2020, toYear: 2021, finalRank: 7},
-                    {fromYear: 2021, toYear: 2022, finalRank: 4},
-                    {fromYear: 2022, toYear: 2023, finalRank: 8}
-                ]
-            }
-        ]
-    }
-]
+  {
+    name: "Premier League",
+    teams: [
+      {
+        fullName: "Arsenal Football Club",
+        nickname: "The Gunners",
+        shortName: "Arsenal",
+        founded: "1886-12-01",
+        ground: {
+          name: "Emirates Stadium",
+          latitude: 51.5550812,
+          longitude: -0.1109831,
+          capacity: 60260,
+        },
+        owner: "Kroenke Sports & Entertainment",
+        manager: "Mikel Arteta",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 5 },
+          { fromYear: 2019, toYear: 2020, finalRank: 8 },
+          { fromYear: 2020, toYear: 2021, finalRank: 8 },
+          { fromYear: 2021, toYear: 2022, finalRank: 5 },
+          { fromYear: 2022, toYear: 2023, finalRank: 2 },
+        ],
+      },
+      {
+        fullName: "Chelsea Football Club",
+        nickname: "The Blues",
+        shortName: "Chelsea",
+        founded: "1905-03-10",
+        ground: {
+          name: "Stamford Bridge",
+          latitude: 51.4816663,
+          longitude: -0.1935314,
+          capacity: 40343,
+        },
+        owner: "Blues Partners Limited",
+        manager: "Mauricio Pochettino",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 3 },
+          { fromYear: 2019, toYear: 2020, finalRank: 4 },
+          { fromYear: 2020, toYear: 2021, finalRank: 4 },
+          { fromYear: 2021, toYear: 2022, finalRank: 3 },
+          { fromYear: 2022, toYear: 2023, finalRank: 12 },
+        ],
+      },
+      {
+        fullName: "Liverpool Football Club",
+        nickname: "The Reds",
+        shortName: "Liverpool",
+        founded: "1892-03-15",
+        ground: {
+          name: "Anfield",
+          latitude: 53.4343003,
+          longitude: -2.9658238,
+          capacity: 53394,
+        },
+        owner: "Fenway Sports Group",
+        manager: "Jürgen Klopp",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 2 },
+          { fromYear: 2019, toYear: 2020, finalRank: 1 },
+          { fromYear: 2020, toYear: 2021, finalRank: 3 },
+          { fromYear: 2021, toYear: 2022, finalRank: 2 },
+          { fromYear: 2022, toYear: 2023, finalRank: 5 },
+        ],
+      },
+      {
+        fullName: "Manchester City Football Club",
+        nickname: "The Citizens",
+        shortName: "Man City",
+        founded: "1880-11-23",
+        ground: {
+          name: "Etihad Stadium",
+          latitude: 53.4831666,
+          longitude: -2.2029485,
+          capacity: 53400,
+        },
+        owner: "City Football Group Limited",
+        manager: "Pep Guardiola",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 1 },
+          { fromYear: 2019, toYear: 2020, finalRank: 2 },
+          { fromYear: 2020, toYear: 2021, finalRank: 1 },
+          { fromYear: 2021, toYear: 2022, finalRank: 1 },
+          { fromYear: 2022, toYear: 2023, finalRank: 1 },
+        ],
+      },
+      {
+        fullName: "Manchester United Football Club",
+        nickname: "The Red Devils",
+        shortName: "Man United",
+        founded: "1878-03-05",
+        ground: {
+          name: "Old Trafford",
+          latitude: 53.4597835,
+          longitude: -2.288024,
+          capacity: 74879,
+        },
+        owner: "Manchester United plc",
+        manager: "Erik ten Hag",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 6 },
+          { fromYear: 2019, toYear: 2020, finalRank: 3 },
+          { fromYear: 2020, toYear: 2021, finalRank: 2 },
+          { fromYear: 2021, toYear: 2022, finalRank: 6 },
+          { fromYear: 2022, toYear: 2023, finalRank: 3 },
+        ],
+      },
+      {
+        fullName: "Tottenham Hotspur Football Club",
+        nickname: "The Lilywhites",
+        shortName: "Spurs",
+        founded: "1882-09-05",
+        ground: {
+          name: "Tottenham Hotspur Stadium",
+          latitude: 51.6042238,
+          longitude: -0.068798,
+          capacity: 62850,
+        },
+        owner: "ENIC International Ltd.",
+        manager: "Ange Postecoglou",
+        players: [],
+        rankHistories: [
+          { fromYear: 2018, toYear: 2019, finalRank: 4 },
+          { fromYear: 2019, toYear: 2020, finalRank: 6 },
+          { fromYear: 2020, toYear: 2021, finalRank: 7 },
+          { fromYear: 2021, toYear: 2022, finalRank: 4 },
+          { fromYear: 2022, toYear: 2023, finalRank: 8 },
+        ],
+      },
+    ],
+  },
+];
